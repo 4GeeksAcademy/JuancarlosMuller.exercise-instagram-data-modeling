1a2f3195d8c22f382dbd3c5e091b5d31d3b4b3b8
Juancarlos Muller Baeza <118220005+JuancarlosMuller@users.noreply.github.com>
2023-08-24 16:10:59 +0000

    final

--- src/models.py
+++ src/models.py
@@ -9,30 +9,33 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'user'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     first_name = Column(String(250), nullable=False)
-    last_name=Column(String(250), nullable=False)
-    email=Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False, unique=True)
     posts = relationship("Post", back_populates="user")
     comments = relationship("Comment", back_populates="user")
+    usersfrom = relationship("Follower", foreign_keys="[Follower.user_from_id]", back_populates="user_from")
+    followers = relationship("Follower", foreign_keys="[Follower.user_to_id]", back_populates="user_to")
 
 class Post(Base):
     __tablename__ = 'post'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship("User", back_populates="posts")
     comments = relationship("Comment", back_populates="post")
 
+class Follower(Base):
+    __tablename__ = 'follower'
+    user_from_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    user_to_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    user_from = relationship("User", foreign_keys=[user_from_id], back_populates="usersfrom")
+    user_to = relationship("User", foreign_keys=[user_to_id], back_populates="followers")
+
 class Comment(Base):
     __tablename__ = 'comment'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id=Column(Integer, primary_key=True)
-    comment_text=Column(String(250), nullable=True)
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(250), nullable=True)
     author_id = Column(Integer, ForeignKey('user.id'))
     post_id = Column(Integer, ForeignKey('post.id'))
     post = relationship("Post", back_populates="comments")
@@ -40,10 +43,10 @@ class Comment(Base):
     def to_dict(self):
         return {}
 
-## Draw from SQLAlchemy base
+# Draw from SQLAlchemy base
 try:
     result = render_er(Base, 'diagram.png')
     print("Success! Check the diagram.png file")
 except Exception as e:
-    print("There was a problem genering the diagram")
+    print("There was a problem generating the diagram")
     raise e
