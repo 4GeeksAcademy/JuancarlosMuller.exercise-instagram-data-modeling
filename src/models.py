57b5f7ea21749f773c40d1647c59c4621d9ab990
Juancarlos Muller Baeza <118220005+JuancarlosMuller@users.noreply.github.com>
2023-08-24 15:02:57 +0000

    first

--- src/models.py
+++ src/models.py
@@ -7,23 +7,35 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    first_name = Column(String(250), nullable=False)
+    last_name=Column(String(250), nullable=False)
+    email=Column(String(250), nullable=False)
+    posts = relationship("Post", back_populates="user")
+    comments = relationship("Comment", back_populates="user")
 
-class Address(Base):
-    __tablename__ = 'address'
+class Post(Base):
+    __tablename__ = 'post'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship("User", back_populates="posts")
+    comments = relationship("Comment", back_populates="post")
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id=Column(Integer, primary_key=True)
+    comment_text=Column(String(250), nullable=True)
+    author_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship("Post", back_populates="comments")
 
     def to_dict(self):
         return {}
